9c160c35dc2a1b5728250b8f29f8ec597d8ed4c0
AhmadRendyFirjatullah <a.rendyfirjatullah@gmail.com>
2023-05-14 15:43:56 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorAssignment.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak di jalankan oleh program
+
+    String nama = "Ahmad Rendy Firjatullah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    }
+
+}
